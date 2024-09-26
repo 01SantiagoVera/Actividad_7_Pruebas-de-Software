1c2c3f7765ed02376078737387e710f65a2b1cda
DaveStev <davidreyes28147@gmail.com>
2024-09-26 14:19:55 -0500

    Se modifico tanto el diseño e los html como los css, js...ademas se creo un nuevo html para calculadora

--- js/Calculate.js
+++ js/Calculate.js
@@ -1,19 +1,23 @@
+function logout() {
+  window.location.href = "index.html"; 
+}
+
 let screen = document.getElementById('screen');
 
 function appendValue(value) {
-  screen.value += value;
+    screen.value += value;
 }
 
 function clearScreen() {
-  screen.value = '';
+    screen.value = '';
 }
 
 function calculate() {
-  try {
-    // Anomalía intencional 1: Sin validación de división entre 0
-    screen.value = eval(screen.value);
-  } catch (error) {
-    // Anomalía intencional 2: No se maneja adecuadamente el error
-    screen.value = 'Error';
-  }
+    try {
+        // Anomalía intencional 1: Sin validación de división entre 0
+        screen.value = eval(screen.value);
+    } catch (error) {
+        // Anomalía intencional 2: No se maneja adecuadamente el error
+        screen.value = 'Error';
+    }
 }
