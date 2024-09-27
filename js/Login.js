b964f75060567294027b03216ea10f544e9f630c
Santiago Vera <130937941+01SantiagoVera@users.noreply.github.com>
2024-09-26 20:50:13 -0500

    Revert "HTML, CSS y JS moificados"

--- js/Login.js
+++ js/Login.js
@@ -1,47 +1,13 @@
 function login() {
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
-  const errorDiv = document.getElementById('error');
 
   // Anomalía intencional 1: No se valida si los campos están vacíos
   if (username === 'admin' && password === '1234') {
-      window.location.href = "dashboard.html";
+    window.location.href = "dashboard.html";
   } else {
-      // Mostrar alerta de error
-      errorDiv.style.display = 'block'; 
-      errorDiv.style.animation = 'fadeIn 0.5s';
-      setTimeout(() => {
-          errorDiv.style.animation = 'fadeOut 0.5s'; 
-          setTimeout(() => {
-              errorDiv.style.display = 'none'; 
-          }, 500); 
-      }, 3000); 
+    document.getElementById('error').style.display = 'block';
   }
 
-  // Anomalía intencional 2: Sin límite en intentos de inicio de sesión
-}
-
-// Boton registro 
-document.getElementById('registerButton').addEventListener('click', function() {
-  const loginContainer = document.getElementById('loginContainer');
-  loginContainer.style.display = loginContainer.style.display === "none" || loginContainer.style.display === "" ? "block" : "none"; 
-});
-
-function togglePassword() {
-  const passwordInput = document.getElementById('password');
-  const passwordIcon = document.getElementById('passwordIcon');
-  if (passwordInput.type === 'password') {
-      passwordInput.type = 'text';
-      passwordIcon.classList.remove('fa-eye');
-      passwordIcon.classList.add('fa-eye-slash'); 
-  } else {
-      passwordInput.type = 'password';
-      passwordIcon.classList.remove('fa-eye-slash');
-      passwordIcon.classList.add('fa-eye'); 
-  }
-}
-
-function closeLogin() {
-  const loginContainer = document.getElementById('loginContainer');
-  loginContainer.style.display = 'none'; 
+// Anomalía intencional 2: Sin límite en intentos de inicio de sesión
 }
